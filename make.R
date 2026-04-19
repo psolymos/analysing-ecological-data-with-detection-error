92bf02f37915d8550a7ef84c1d642f8128c022fa
Peter Solymos <psolymos@gmail.com>
2026-04-19 17:39:36 -0600

    Update make
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- make.R
+++ make.R
@@ -1,12 +1,21 @@
+# Render qmd files
 # Rscript --vanilla make.R
 # Rscript --vanilla make.R --day 1
+# Rscript --vanilla make.R --session 01
+# Rscript --vanilla make.R --session 01 --format beamer
 
 CONFIG <- rconfig::rconfig()
 str(CONFIG)
 
-DAY <- rconfig::value(CONFIG$day, "0")
+FORMAT <- rconfig::value(CONFIG$format, "all")
 
-dir <- switch(
+if (!is.null(CONFIG$day) && !is.null(CONFIG$session)) {
+  stop("Cannot specify both day and session")
+}
+
+if (!is.null(CONFIG$day)) {
+  DAY <- rconfig::value(CONFIG$day, "0")
+  dir <- switch(
     as.character(DAY[1]),
     "0" = ".",
     "1" = "day-01",
@@ -14,11 +23,29 @@ dir <- switch(
     "3" = "day-03",
     "4" = "day-04",
     stop("Invalid day argument")
-)
-fl <- list.files(dir, recursive = TRUE)
-fl <- fl[grep("\\.qmd$", fl)]
+  )
+  fl <- list.files(dir, recursive = TRUE)
+  fl <- fl[grep("\\.qmd$", fl)]
+}
 
-# Render qmd files
+if (!is.null(CONFIG$session)) {
+  SESSION <- rconfig::value(CONFIG$session, "0")
+  dir <- "."
+  fl <- list.files(dir, recursive = TRUE)
+  fl <- fl[grep("\\.qmd$", fl)]
+  fl <- fl[grep(SESSION, fl)]
+}
+
+OK <- logical(length(fl))
 for (i in fl) {
-    try(quarto::quarto_render(file.path(dir, i), output_format = "all"))
+  r <- try(quarto::quarto_render(file.path(dir, i), output_format = FORMAT))
+  OK[i == fl] <- !inherits(r, "try-error")
+}
+
+if (any(!OK)) {
+  warning("Some files failed to render:")
+  cat(fl[!OK], sep = "\n")
+  cat("\n")
+} else {
+  message("All files rendered successfully.")
 }
